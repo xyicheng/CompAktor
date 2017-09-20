bf514ea16f65b11a194c8598aa8eb4f426a0b865
asevans48 <asevans48@gmail.com>
2017-09-19 23:41:33 -0600

    [streams] added a test class for the sterams

---
+++ test/streams.py
@@ -0,0 +1,42 @@
+'''
+Stream tests.
+
+Created on Sep 19, 2017
+
+@author: aevans
+'''
+
+
+def test_stream_intantiation():
+    """
+    Test stream instantiation. 
+    """
+    pass
+
+
+def test_text_tiling_flow():
+    """
+    Directory specific.  Inputs data as outlined here.
+    Download by searching American Poli at
+    https://app.scrapinghub.com/datasets
+    """
+    pass
+
+def test_at_load():
+    """
+    Test data at load
+    """
+    pass
+
+
+def test_multi_flow():
+    """
+    Test mltiple flows (sub-flows)
+    """
+    pass
+
+def test_multi_flow_at_load():
+    """
+    Test mutltiple flows against a heavy load
+    """
+    pass
