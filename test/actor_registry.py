acdc3d154a362a238985f5118da2983074bbc298
asevans48 <asevans48@gmail.com>
2017-10-15 07:37:34 -0600

    [testing] Added more tests. Testing ETL and registry usage.

--- test/actor_registry.py
+++ test/actor_registry.py
@@ -6,11 +6,32 @@ Created on Oct 14, 2017
 
 import pytest
 from test.actor import StringTestActor
+from compaktor.state.actor_state import ActorState
 from compaktor.registry.actor_registry import Registry
 
 
-def test_add_actor():
-    registry = Registry()
+def test_add_actor_at_root():
+    registry = Registry("localhost")
     testa = StringTestActor("string_test")
     testa.start()
-    registry.add_actor("root/testa", actor, is_local)
+    addr = testa.get_address()
+    registry.add_actor("localhost", testa, is_local=True)
+    test_arr = [x for x in addr if x not in ["localhost","testa"]]
+    assert(len(test_arr) is 0)
+    assert(testa.get_address() != None)
+    node = registry.find_node(testa.address)
+    assert(node != None)
+    registry.stop_all()
+    assert(testa.get_state() == ActorState.STOPPED)
+
+
+def test_multi_level_actor():
+    pass
+
+
+def test_load():
+    pass
+
+
+if __name__ == "__main__":
+    test_add_actor_at_root()
